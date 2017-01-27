88caa7398a7863f6df411b24ee29718f4be03984
dmozzoni <cyric@hotmail.com>
2017-01-27 00:53:08 -0500

    cleanup and readme

--- app.js
+++ app.js
@@ -7,47 +7,58 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+
 var mongoose = require('mongoose');
 require('./models/Posts');
 require('./models/Comments');
-var mongoURI =  process.env.MONGODB_URI || 'mongodb://localhost/beer_api';
+var mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/beer_api';
 mongoose.connect(mongoURI);
 mongoose.Promise = global.Promise;
-// mongoose.connect('mongodb://localhost/news');
+
 var app = express();
 app.use(cors());
+
+
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', index);
-app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
+
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    // res.status(err.status || 500);
+    // res.render('error');
+    res.json({
+        'error': err.message
+    });
 });
+
 
 module.exports = app;
--- models/Posts.js
+++ models/Posts.js
@@ -4,28 +4,38 @@ var CommentSchema = require('./Comments.js');
 var Comment = mongoose.model('Comment');
 
 var PostSchema = new mongoose.Schema({
-  title: String,
-  link: String,
-  upvotes: {type: Number, default: 0},
-  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
+    title: String,
+    link: String,
+    upvotes: {
+        type: Number,
+        default: 0
+    },
+    comments: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Comment'
+    }]
 });
 
+// Method to increment upvote on posts
 PostSchema.methods.upvote = function(cb) {
-  this.upvotes += 1;
-  this.save(cb);
+    this.upvotes += 1;
+    this.save(cb);
 };
 
-// removes comment reference from post's comment array after a comment delete
+// Removes comment reference from post's comment array after a comment delete
 PostSchema.methods.removeCommentRef = function(cref, cb) {
-  this.comments = this.comments.filter(function(val){
-    return val != cref.toString();
-  });
-  this.save(cb);
+    this.comments = this.comments.filter(function(val) {
+        return val != cref.toString();
+    });
+    this.save(cb);
 };
 
+// Removes all child comment references before removing a post
 PostSchema.pre('remove', function(next) {
-  Comment.remove({post: this._id}).exec();
-  next();
+    Comment.remove({
+        post: this._id
+    }).exec();
+    next();
 });
 
 mongoose.model('Post', PostSchema);
